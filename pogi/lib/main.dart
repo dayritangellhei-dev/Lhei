d7fe6723afc97febbe4734da4a48a27ffa28dae2
dayritangellhei-dev <dayrit.angellhei@gmail.com>
2026-09-11 09:21:05 +0000

    changing code

--- pogi/lib/main.dart
+++ pogi/lib/main.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
-
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-    @override
-      Widget build(BuildContext conext){
-        return const MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: Scaffold(
-            body: Center(
-              child: Text('Hello World')
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Angel Lhei D. Dayrit'),Text('Bachelor of Science Information Technology'),
+              ],
+          ),
+          
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
